cc18d8e8966c5a699d4cddccae8470bcea56ded9
MetaVision2004 <serjegomare@gmail.com>
2026-03-25 14:16:12 -0500

    Rename MYSQL_DATABASE to MYSQL_DB

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ class Config:
     MYSQL_USER = "root"
     MYSQL_PASSWORD = "TiSiWZQcDcEldqSlAKhUgLexMCQCnpEc"
     MYSQL_PORT = 3306
-    MYSQL_DATABASE = "railway"
+    MYSQL_DB = "railway"
     SECRET_KEY = "3f82b7c6a9d4e1f50c38a29b47d6e8f15a9c2b4d7e0f3a6c9b148d5e2f7a0c3b"
