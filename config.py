7deca9fcb7c3c2a75b8cd3a4cfbe374a65c0f46f
MetaVision2004 <serjegomare@gmail.com>
2026-03-25 14:50:05 -0500

    cambiar resulta

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
+import os
+
 class Config:
-    MYSQL_HOST = "mysql.railway.internal"
-    MYSQL_USER = "root"
-    MYSQL_PASSWORD = "TiSiWZQcDcEldqSlAKhUgLexMCQCnpEc"
-    MYSQL_PORT = 3306
-    MYSQL_DB = "railway"
+    MYSQL_HOST = os.getenv("MYSQLHOST")
+    MYSQL_USER = os.getenv("MYSQLUSER")
+    MYSQL_PASSWORD = os.getenv("MYSQLPASSWORD")
+    MYSQL_PORT = int(os.getenv("MYSQLPORT", "3306"))
+    MYSQL_DB = os.getenv("MYSQLDATABASE")
     SECRET_KEY = "3f82b7c6a9d4e1f50c38a29b47d6e8f15a9c2b4d7e0f3a6c9b148d5e2f7a0c3b"
